475da283e99d42bc5895fe5bec74ac4a699e5171
Serhiy <serhiylaba@gmail.com>
2023-05-12 12:15:35 +0300

    Initial commit

---
+++ App/js/script.js
@@ -0,0 +1,24 @@
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count:numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat:false
+}
+
+const a = prompt('Один из последних просмотренных фильмов? ', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из просмотренныз фильмов?', '' ),
+      d = prompt('На сколько оцените его?' , '');
+      z = prompt('На сколько оцените его?' , '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[b] = c;
+
+sdffdf
+
+
+console.log(personalMovieDB);
