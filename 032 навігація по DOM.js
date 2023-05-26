c0f226e5d785cc8ef63ed8fe77b05d4bb0074bf9
Serhiy <serhiylaba@gmail.com>
2023-05-26 17:45:52 +0300

    29's commit

---
+++ 032 навігація по DOM.js
@@ -0,0 +1,16 @@
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+
+console.log(document.querySelector('#current').parentNode.parentNode);
+
+console.log(document.querySelector('[data-current="3"]'));
+
+for (let node of document.body.childNodes) {
+if (node.nodeName == '#text') {
+    continue;
+}
+    console.log(node);
+}
