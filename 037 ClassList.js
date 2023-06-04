192e3c4c87bde916369b2130e7e39e005cd5d01d
Serhiy <serhiylaba@gmail.com>
2023-06-04 18:32:41 +0300

    35s commit

--- 037 ClassList.js
+++ 037 ClassList.js
@@ -1,4 +1,5 @@
-const btns = document.querySelectorAll('button');
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
 
 //console.log(btns[0].classList.length);
 //console.log(btns[0].classList.item(1));
@@ -22,6 +23,27 @@ btns[0].addEventListener('click', () => {
 
 });
 
-console.log(btns[0].className);
+// console.log(btns[0].className);
 
 // 11:24 время. Делегирование событий
+
+
+wrapper.addEventListener('click', (event1) => {
+    if (event1.target && event1.target.matches("button.red") ) {
+        console.log('Hello');
+    }
+});
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('Hello');
+//     });
+// });
+
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
+
+
+
