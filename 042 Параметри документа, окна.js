e167f0d67c0dd7f3d17890a26cac93ad2d2f1c5d
Serhiy <serhiylaba@gmail.com>
2023-06-05 18:07:06 +0300

    36's commit

---
+++ 042 Параметри документа, окна.js
@@ -0,0 +1,11 @@
+// document
+
+// window  - єто окно в котором пока зивается документ;
+
+// screen єкран - єто ввесь видимий монитор
+
+const box = document.querySelector('.box');
+
+const width = box.clientWidth;
+const height = box.clientHeight;
+console.log(width, height);
