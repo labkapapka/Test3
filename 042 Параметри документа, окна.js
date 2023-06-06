6f431a74e6086ee827e48cdcb7d0e6e7270c2499
Serhiy <serhiylaba@gmail.com>
2023-06-06 17:47:49 +0300

    37's commit

--- 042 Параметри документа, окна.js
+++ 042 Параметри документа, окна.js
@@ -3,9 +3,27 @@
 // window  - єто окно в котором пока зивается документ;
 
 // screen єкран - єто ввесь видимий монитор
+'use strict';
+const box = document.querySelector('.box'),
+      btn = document.querySelector('button');
+      
+// const width = box.clientWidth;
+// const height = box.clientHeight;
+// const width = box.offsetWidth;
+// const height = box.offsetHeight;
 
-const box = document.querySelector('.box');
-
-const width = box.clientWidth;
-const height = box.clientHeight;
+const width = box.scrollWidth;
+const height = box.scrollHeight;
 console.log(width, height);
+
+btn.addEventListener('click', () => {
+    // box.style.height = box.scrollHeight + 'px';
+    console.log(box.scrollTop);    
+});
+
+console.log(box.getBoundingClientRect().top);
+
+const style = window.getComputedStyle(box);
+console.log(style.display);
+
+console.log(document.documentElement.scrollTop);
